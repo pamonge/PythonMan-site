30d989d2dc46b02dcad7047bdb77fd0528a59692
pamonge <pablomonge55@gmail.com>
2024-10-02 01:17:55 -0300

    Add: biograficomponent

---
+++ src/components/BiografiComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './BiografiComponentStyles.module.css'
+
+export const BiografiComponent = () => {
+  return (
+    <div className={styles.container}>
+        BiografiComponent
+    </div>
+  )
+}
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from './HomePageStyles.module.css'
 import pythonBackImg from '../../assets/images/site imgs/Big transparent Python Man.png'
+import { BiografiComponent } from '../../components/BiografiComponent'
 
 
 export const HomePage = () => {
   return (
     <div className={styles.container}>
-        <img src={pythonBackImg} alt="Logo" />
+        <img className={styles.initialBigLogo} src={pythonBackImg} alt="Logo" />
+        <BiografiComponent />
     </div>
   )
 }
