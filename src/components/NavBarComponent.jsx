624e6799ed658350df8e145f370cbf571a5c0639
pamonge <pablomonge55@gmail.com>
2024-10-02 01:03:23 -0300

    Refactor: Site navbar, homepage

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -5,39 +5,31 @@ import pythonManLogo from '../assets/images/site imgs/python man.png'
 
 export const NavBarComponent = () => {
   return (
-    <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img src={ pythonManLogo } ></img>
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink to="/" className="nav-link active" aria-current="page" >Inicio</ NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/about" className="nav-link" >Acerca de</ NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/blog" className="nav-link" >Blog</ NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/booking" className="nav-link" >Booking</ NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/store" className="nav-link" >Tienda</ NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/contact" className="nav-link" >Contacto</ NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </>
+    <div className={styles.container}>
+        
+        <span className={ styles.pythonLogo }>
+            <img src={ pythonManLogo } alt='python man logo'></img>
+        </span>
+        <ul className={styles.navLinksList}>
+            <li className={styles.navItem}>
+                <NavLink to="/" className="">INICIO</ NavLink>
+            </li>
+            <li className={styles.navItem}>
+                <NavLink to="/about" className={styles.navLink} >ACERCA DE</ NavLink>
+            </li>
+            <li className={styles.navItem}>
+                <NavLink to="/blog" className={styles.navLink} >BLOG</ NavLink>
+            </li>
+            <li className={styles.navItem}>
+                <NavLink to="/booking" className={styles.navLink} >BOOKING</ NavLink>
+            </li>
+            <li className={styles.navItem}>
+                <NavLink to="/store" className={styles.navLink} >TIENDA</ NavLink>
+            </li>
+            <li className={styles.navItem}>
+                <NavLink to="/contact" className={styles.navLink} >CONTACTO</ NavLink>
+            </li>
+        </ul>
+    </div>
   )
 }
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './HomePageStyles.module.css'
+import pythonBackImg from '../../assets/images/site imgs/Big transparent Python Man.png'
+
 
 export const HomePage = () => {
   return (
-    <div>
-        <h1>HomePage</h1>
+    <div className={styles.container}>
+        <img src={pythonBackImg} alt="Logo" />
     </div>
   )
 }
