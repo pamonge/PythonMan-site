8e05f23ba360e19e9044b670873f9fa30e61775d
pamonge <pablomonge55@gmail.com>
2024-10-03 14:40:00 -0300

    Reafactor: biography, homepage

---
+++ src/components/BiographyComponent.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './BiographyComponentStyles.module.css'
+import clockImg from '../assets/images/backgrounds/clock.jpeg'
+
+export const BiographyComponent = () => {
+  return (
+    <div className={styles.container}>
+		<div className={styles.textSection}>
+			<h3 className={styles.secTittle}>BIOGRAFÍA</h3>
+			<p>
+				Mi nombre es Adolfo González Hernández, y desde 2021 incursioné en el sector TI de una forma casi casual,    habiendo sido mánager de proyectos editoriales 
+				en Estudio Silveria, S.L. desde el año 2004 hasta 
+				la fecha anteriormente mencionada.
+			</p>
+			<p>
+				Mi curiosidad por aprender programación me llevaría a descubrir Python, sus increíbles frameworks y convertirme en desarrollador multiplataforma en disciplinas como Desarrollo Web, Análisis de Datos, Machine Learning y Ciberseguridad.
+			</p>
+			<p>
+				Junto a mi equipo, antiguos colaboradores expertos en FrontEnd, Diseño UX y Análisis de Datos, estamos iniciando una nueva aventura como consultoría informática freelance para pymes y autónomos, habiendo sido previamente un equipo plenamente consolidado en un proyecto de gran envergadura, Alimenta Forma.
+			</p>
+		</div>
+		<div className={styles.imgContainer}>
+			<img className={styles.biographyClock} src={clockImg} alt="clock" />
+		</div>
+    </div>
+  )
+}
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import styles from './HomePageStyles.module.css'
 import pythonBackImg from '../../assets/images/site imgs/Big transparent Python Man.png'
-import { BiografiComponent } from '../../components/BiografiComponent'
+import { BiographyComponent } from '../../components/BiographyComponent'
+
 
 
 export const HomePage = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.overlay}>
         <img className={styles.initialBigLogo} src={pythonBackImg} alt="Logo" />
-        <BiografiComponent />
+        <BiographyComponent />
+      </div>
     </div>
   )
 }
