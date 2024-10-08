27ed0ecf548bb438616bc3636e2908323cac0100
pamonge <pablomonge55@gmail.com>
2024-10-08 14:30:18 -0300

    Add: TeamComponent

---
+++ src/components/TeamComponent.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import styles from './TeamComponentStyles.module.css'
+import abril from '../assets/images/team/abril.png'
+import adolfo from '../assets/images/team/adolfo.png'
+import mirella from '../assets/images/team/mirella.png'
+import pablo from '../assets/images/team/pablo.png'
+import pedro from '../assets/images/team/pedro.png'
+import veronica from '../assets/images/team/veronica.png'
+
+export const TeamComponent = () => {
+  return (
+    <div className={styles.teamContainer}>
+        <div>
+            <img src={pablo} alt="Pablo Monge" />
+            <p>PABLO MONGE</p>
+            <p>
+                Tech Lead, FrontEnd Developer, BackEnd Developer, QA Tester, Linux Technician (Level Two)
+            </p>
+        </div>
+        <div>
+            <img src={abril} alt="Abril Gonzalez" />
+            <p>ABRIL GONZÁLEZ</p>
+            <p>
+                UX/UI Designer, Graphic Designer and Graduate in Business Image Design
+            </p>
+        </div>
+        <div>
+            <img src={veronica} alt="Veronica Sanchez Justicia" />
+            <p>VERÓNICA SÁNCHEZ JUSTICIA</p>
+            <p>
+                BackEnd Developer, FrontEnd Developer, System Administrator
+            </p>
+        </div>
+        <div>
+            <img src={adolfo} alt="Adolfo Gonzalez Hernanddez" />
+            <p>ADOLFO GONZÁLEZ HERNÁNDEZ</p>
+            <p>
+                Jefe de Proyectos y Scrum Master, Python Generalist (Level Two), Data Scientist, Data Analist, Machine Learning Engineer, Quantum Holographic Engineer (Level One)
+            </p>
+        </div>
+        <div>
+            <img src={pedro} alt="Pedro Duran" />
+            <p>PEDRO DURÁN</p>
+            <p>
+                Data Analist, Data Scientist, Pentester, Cibersecurity Engineer
+            </p>
+        </div>
+        <div>
+            <img src={mirella} alt="Mirella Picolli" />
+            <p>MIRELLA PICOLLI</p>
+            <p>
+                Data Analist, Data Scientist, Power BI Analist, Junior Machine Learning
+            </p>
+        </div>
+    </div>
+  )
+}
--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import styles from './AboutPageStyles.module.css'
+import { TeamComponent } from '../../components/TeamComponent'
+
 
 export const AboutPage = () => {
-  return (
-    <div>AboutPage</div>
-  )
+
+return (
+	<div className={styles.aboutContainer}>
+		<div className={styles.textContainer}>
+			<h3>NUESTRO EQUIPO</h3>
+			<p>DEDICACIÓN. EXPERIENCIA. PASIÓN POR LO QUE HACEMOS. COMPROMETIDOS CON LA EXCELENCIA</p>
+			<p>
+				Este es el actual equipo de colaboradores y desarrolladores o especialistas en diversas disciplinas en PYTHON MAN. Aquí mostramos una breve descripción de todos ellos, sin excluir a ninguno, como una pequeña biografía de sus habilidades y destrezas profesionales:
+			</p>	
+		</div>
+		<TeamComponent />
+	</div>
+)
 }
