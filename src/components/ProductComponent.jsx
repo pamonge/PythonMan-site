25b5720228223d827e196b9ca45e4270c636a96a
Flia <flia@debian.Flia>
2024-10-22 14:31:39 -0300

    Refactor: productsComponent

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -5,8 +5,24 @@ import dataAnalisis from '../assets/images/general imgs/dataAnalisisProduct.png'
 
 export const ProductComponent = () => {
   return (
-    <div>
+    <div className={styles.productContainer}>
         <h3>Productos</h3>
+        <div className={styles.productDescription}>
+            <div className={styles.product}>
+                <img src={dataAnalisis} alt="Data Analisis" />
+                <h4>ANALISIS DE DATOS</h4>
+                <p>Permite contar con información oportuna y precisa al momento de tomar deciciones de negocios.</p>
+            </div>
+            <div className={styles.product}>
+                <img src={basicWeb} alt="Web Site" />
+                <h4>DISEÑO DE SITIOS Y APLICACIONES WEB</h4>
+                <p>Permite acceso al sistema desde cualquier plataforma en cualquier lugar y momento.</p>
+            </div>
+            <div className={styles.product}>
+                <p>aproveche al máximo el poder de la web y los datos para crecer de manera inteligente.</p>
+                <p>Nosotros lo ayudamos a lograrlo, con soluciones personalizadas que impulsen su éxito.</p>
+            </div>
+        </div>
     </div>
   )
 }
